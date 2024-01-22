19db4669bcd0f97b11888bd8b10f9ae12b9a4740
Muratcan Gözüm <murat.can1250@gmail.com>
2024-01-22 19:53:57 +0300

    some scss fixes

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,19 +1,21 @@
 import {Outlet, Link} from "react-router-dom";
 import {Fragment} from "react";
 import {ReactComponent as CrownLogo} from "../../../assets/crown.svg";
+import "./navigation.styles.scss"
 
 export const Navigation = () => {
 
     return (
         <Fragment>
             <div className={"navigation"}>
-                <Link className={"nav-logo"} to={"/"}>
+                <Link className={"logo-container"} to={"/"}>
                     <CrownLogo/>
                 </Link>
-                <div className={"nav-links-container"}></div>
-                <Link className={"nav-link"} to={"/mağaza"}>
-                    Shop
-                </Link>
+                <div className={"nav-links-container"}>
+                    <Link className={"nav-link"} to={"/mağaza"}>
+                        Mağaza
+                    </Link>
+                </div>
             </div>
             <Outlet/>
         </Fragment>
